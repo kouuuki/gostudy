244370b7555ba446070655d3e4ae81fb28110e88
kouuuki <mark.web.888@gmail.com>
2018-04-15 22:55:45 +0900

    commit2

--- hello.go
+++ hello.go
@@ -5,5 +5,17 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("hello")
+  var msg string
+  msg = "hello"
+
+  var msg2 = "world"
+  msg3 := "hello world"
+  
+  fmt.Println(msg)
+  fmt.Println(msg2)
+  fmt.Println(msg3)
+  fmt.Println(false)
+  fmt.Println(true)
+  fmt.Println(1 + 1 * 4)
+
 }
