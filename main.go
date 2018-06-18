8850ab26430d1ac79df96d2441f62acfa940dd6c
kouuuki <mark.web.888@gmail.com>
2018-06-18 16:05:52 +0900

    add go file

---
+++ main.go
@@ -0,0 +1,14 @@
+// Goの勉強
+
+package main
+
+import (
+   "fmt"
+   "./animal"
+ )
+
+func main() {
+  fmt.Println(animal.ElephantFeed())
+  fmt.Println(animal.MankeyFeed())
+  fmt.Println(animal.RabbitFeed())
+}
